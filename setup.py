35cdf940f70a2931c31ee3e7d702d72616048430
Adam Romano <manoadamro.imp@gmail.com>
2019-01-10 19:31:56 +0000

    repo setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+import setuptools
+
+
+NAME = 'lazy_python_repo'
+VERSION = '0.0.0'
+REQUIRES = [
+
+]
+
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    install_requires=REQUIRES,
+    packages=setuptools.find_packages()
+)
